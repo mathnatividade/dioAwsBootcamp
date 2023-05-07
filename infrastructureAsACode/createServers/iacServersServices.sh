9aa2e681ecad68b733256daff581f298961568ae
mathnatividade <mathnatividade@gmail.com>
2023-05-06 23:58:47 -0300

    Criação script subir servidor apache com aplicação

--- infrastructureAsACode/createServers/iacServersServices.sh
+++ infrastructureAsACode/createServers/iacServersServices.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#Instalar Samba
-sudo apt install samba -y
-
-#Criar pasta de compartilhamento
-cd /
-sudo mkdir /disk2
-cd /disk2
-sudo mkdir /publico
-sudo chmod 777 publico
-#sudo nano /etc/samba/smb.conf
-cd ~
-sudo cat smbConf.txt >> /etc/samba/smb.conf
-sudo systemctl restart smbd
-sudo systemctl enable smbd
+#Atualizar sistema
+echo "Atualizando sistema"
+sudo apt update
+sudo apt upgrade -y
 
 #Instalar Apache
+echo "Instalando Apache"
 sudo apt install apache2 -y
 
-#Instalar MySQL
-sudo apt instal mysql-server-8.0
+#Instalar unzip
+echo "Instalando unzip"
+sudo apt install unzip -y
 
-
+#Acessar pasta temp
+echo "Obtendo arquivos do site"
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+sudo cp -r * /var/www/html/
+sudo service apache2 start
+echo "Arquivos obtidos com sucesso, site no ar"
